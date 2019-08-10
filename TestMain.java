1619f15c32020f401fa8724a124d16471ddf3178
ZL0101 <31301959+ZL0101@users.noreply.github.com>
2019-08-10 11:51:27 +0800

    Initial commit

---
+++ TestMain.java
@@ -0,0 +1,13 @@
+package com.zl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TestMain {
+    public static void main(String[] args) {
+        ArrayList a=new ArrayList<>();
+        HashMap b=new HashMap<>();
+    }
+
+
+}
